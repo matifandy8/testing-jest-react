8a1c7f8dd8cf4e790a938347bc343202788e93f5
matias <matifandy@gmail.com>
2021-08-06 12:14:15 -0300

    testing form

--- src/App.js
+++ src/App.js
@@ -1,23 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.css';import { Formik, Form, Field, ErrorMessage } from "formik";
+import DatePicker from "react-datepicker";
+import "react-datepicker/dist/react-datepicker.css";
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <Formik
+    initialValues={{ email: "", date: "" }}
+    validate={values => {
+      let errors = {};
+      if (!values.date) {
+        errors.date = "Required";
+      }
+      if (!values.email) {
+        errors.email = "Required";
+      }
+      return errors;
+    }}
+  >
+    {({ isSubmitting, setFieldValue, values }) => (
+      <Form>
+        <label htmlFor="email">Email</label>
+        <Field id="email" type="email" name="email" />
+        <ErrorMessage data-testid="emailError" name="email" component="div" />
+        <br />
+        <DatePicker
+          selected={values.date}
+          onChange={event => {
+            setFieldValue("date", event);
+          }}
+        />
+        <ErrorMessage data-testid="dateError" name="date" component="div" />
+        <br />
+        <button type="submit">Submit</button>
+      </Form>
+    )}
+  </Formik>
     </div>
   );
 }
--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,37 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
+import React from "react";
+import "@testing-library/jest-dom/extend-expect";
+import { render, fireEvent, wait } from "@testing-library/react";
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+
+it("should show validation on blur", async () => {
+  const { getByLabelText, getByTestId } = render(<App />);
+  const input = getByLabelText("Email");
+  fireEvent.blur(input);
+  await wait(() => {
+    expect(getByTestId("emailError")).not.toBe(null);
+    expect(getByTestId("emailError")).toHaveTextContent("Required");
+  });
+});
+
+
+jest.mock("react-datepicker", () => props => (
+  <input
+    data-testid="mockedDateField"
+    onChange={e => {
+      props.onChange(e);
+    }}
+  />
+));
+
+test("should remove date error id we select date", async () => {
+  const { getByText, getByTestId, queryByTestId } = render(<App />);
+  const button = getByText("Submit");
+  fireEvent.click(button);
+  const mockedDateField = getByTestId("mockedDateField");
+  fireEvent.change(mockedDateField, { target: { value: new Date() } });
+  await wait(() => {
+    expect(queryByTestId("dateError")).toBe(null);
+  });
 });
